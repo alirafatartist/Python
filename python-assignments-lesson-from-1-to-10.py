79ef40cc1fc4343597ad8b67683b818e58a8a6d3
ᗩᒪᓰ_ <89305319+alirafatartist@users.noreply.github.com>
2024-05-27 02:43:40 +0300

    Create python-assignments-lesson-from-1-to-10.py

---
+++ python-assignments-lesson-from-1-to-10.py
@@ -0,0 +1,27 @@
+# Assignment 1
+"""
+This is a comment
+"""
+
+# Assignment 2
+Name = "Ali"
+age = "10"
+Country ='Egypt'
+
+# Assignment 3
+print("Name: "+Name +"\n"+ "age: "+age +"\n"+ "Country: "+Country)
+# Name: Ali
+# age: 10
+# Country: Egypt
+
+# Assignment 4
+print("Hello, My Name Is " + Name + " And I am " + age + " Years Old and I Live in "+ Country)
+# Hello, My Name Is Ali And I am 10 Years Old and I Live in Egypt
+
+# Assignment 5
+print(type(Name))
+print(type(age))
+print(type(Country))
+# <class 'str'>
+# <class 'str'>
+# <class 'str'>
