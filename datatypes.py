b8f9c4ff188a1bac8f4aa9d0b16825ebec1b7776
ᗩᒪᓰ_ <89305319+alirafatartist@users.noreply.github.com>
2024-05-27 01:10:21 +0300

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,14 @@
+print(type(10));
+# int
+print(type(10.9));
+# float
+print(type("Hello python"));
+# string
+print(type([1,2,3,4,5]));
+# list
+print(type((1,2,3,4,5)));
+# tuple 
+print(type({"x":3,"y":6,"z":-1}));
+# dict
+print(type( 7 == 7));
+# boolean
