252ffe60667fab9e276c7308f09b24d46a41a60e
ᗩᒪᓰ_ <89305319+alirafatartist@users.noreply.github.com>
2024-05-27 01:09:40 +0300

    Create First.py

---
+++ First.py
@@ -0,0 +1,16 @@
+print("i love python")
+print("i love programming")
+# i love python
+# i love programming
+
+
+print("i love python"); print("i love programming")
+# i love python
+# i love programming
+
+    print("i love python")
+    print("i love programming")
+"""
+Error
+IndentationError: unexpected indent
+"""
