4a4c1de13d6cdcf7f8a79abd02fffb0e9379be6f
alirafatartist <lraft636@gmail.com>
2025-06-27 16:53:47 +0300

    easy-7

---
+++ Algorithms/Easy/7.py
@@ -0,0 +1,12 @@
+def filter_even_numbers(list):
+    result =[]
+    for i in list:
+        if i % 2 == 0:
+            result.append(i)
+
+    return result
+
+
+print(filter_even_numbers([1, 2, 3, 4, 5]))
+
+
